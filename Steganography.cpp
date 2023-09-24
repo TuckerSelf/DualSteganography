6c5058da268c41933405c4ea94520e033251d000
TuckerSelf <tself3@ut.utm.edu>
2023-09-24 17:14:07 -0500

    doesn't work but it's a start

--- Steganography.cpp
+++ Steganography.cpp
@@ -16,21 +16,54 @@
 using namespace std;
 
 int Steganography::getNthBit(char cipherChar, int n){
+
 }
 
 void Steganography::readImage(string fileName){
+  ifstream inFile;
+  string imageData;
+  inFile.open(fileName);
+  
+  getline(inFile, magicNumber);
+  inFile >> width >> height;
+  getline(inFile, ignore);
+  inFile >> maxColor;
+  
+  for(int i=0; inFile; i++){
+    getline(inFile, imageData);
+    colorData[i] = imageData;
+  }
+  inFile.close();
 }
 
 void Steganography::printImage(string fileName){
+  ofstream outFile;
+  outFile.open(fileName);
+  outFile << magicNumber;
+  outFile.close();
 }
 
 void Steganography::readCipherText(string fileName){
+  ifstream inFile;
+  string imageData;
+  inFile.open(fileName);
+  getline(inFile, imageData);
+  cipherText = imageData;
+  while(inFile){
+    getline(inFile, imageData);
+    cipherText += imageData;
+  }
+  inFile.close();
 }
 
 void Steganography::printCipherText(string fileName){
 }
 
 void Steganography::cleanImage(){
+  for(int i=0; i<(width*height); i++){
+    if(colorData[i]%2!=0)
+      colorData[i]-=1;
+  }
 }
 
 void Steganography::encipher(){
