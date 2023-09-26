0ecc3190844ab06610c2397b7cdeff1c0f53b564
Braden Windsor <bwindso1@ut.utm.edu>
2023-09-25 21:47:13 -0500

    Added main functionality, can't test

--- main.cpp
+++ main.cpp
@@ -15,7 +15,55 @@
 using namespace std;
 
 int main() {
+  Steganography notmySteganography;
+  char userChoice = '-';
+  int check = 0;
+  string ppmName, inputText, encodedImg, outputText;
+
+
   
-  
+  while(check != 2){
+
+    cout << "Please choose your operation.\nE for encoding, D for decoding: ";
+    cin >> userChoice;
+    
+    if((userChoice == 'e') || (userChoice == 'E')){
+      cout << "Please enter the PPM, input text file, and the encoded image file names in that order\n";
+      cin >> ppmName;
+
+      cout << "\n";
+      cin >> inputText;
+
+      cout << "\n";
+      cin >> encodedImg;
+
+      notmySteganography.readImage(ppmName);
+      notmySteganography.readCipherText(inputText);
+
+      notmySteganography.encipher();
+      notmySteganography.printImage(encodedImg);
+      
+      check = 2;
+    }
+
+    else if((userChoice == 'd') || (userChoice == 'D')){
+      cout << "Please enter the PPM and the output text file names in that order\n";
+      cin >> ppmName;
+
+      cout << "\n";
+      cin >> outputText;
+      
+      notmySteganography.readImage(ppmName);
+      notmySteganography.decipher();
+      notmySteganography.printCipherText(outputText);
+      
+      check = 2;
+    }
+    
+    else{
+      cout << "Please enter valid input\n\n";
+    }
+    
+  } 
   return 0;
 }
